241d5d9dce378d464b160ad30ff4897e3d91fe85
InagakiKumi <k_inagaki@freestyles.jp>
2020-02-10 00:10:05 +0900

    "[2020/02/09] @0.5h Main.csの作成"

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public static void main(String[] args)
+	{
+		GameManager gm = new GameManager();
+		gm.Start();
+	}
+}
