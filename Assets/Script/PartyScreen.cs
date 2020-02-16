214f55aecb611faa68967ae6f831b83a694f4dca
InagakiKumi <k_inagaki@freestyles.jp>
2020-02-17 01:09:13 +0900

    "[2020/02/16] @1h パーティー編成画面ファイルの作成"

---
+++ Assets/Script/PartyScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PartyScreen : MonoBehaviour
+{
+    public void ButtonClick()
+    {
+        switch (transform.name)
+        {
+            case "BackButton":
+                Debug.Log("「戻る」を押した");
+                SceneManager.LoadScene("TopScreen");
+                break;
+            case "StartButton":
+                Debug.Log("「このパーティーで開始」を押した");
+                SceneManager.LoadScene("BattleStart");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+}
