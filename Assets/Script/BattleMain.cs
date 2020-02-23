203aa01e06c7271a6c08bdcd6e63a576b69128ca
InagakiKumi <k_inagaki@freestyles.jp>
2020-02-23 23:56:41 +0900

    "[2020/02/23] @1h バトルメイン画面ファイルの修正"

---
+++ Assets/Script/BattleMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleMain : MonoBehaviour
+{
+    public void ButtonClick()
+    {
+        switch (transform.name)
+        {
+            case "ChangeButton":
+                Debug.Log("「変更」を押した");
+                SceneManager.LoadScene("ChangeStrategy");
+                break;
+            case "NextButton":
+                Debug.Log("「次のターン」を押した");
+                break;
+            default:
+                break;
+        }
+    }
+}
