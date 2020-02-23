690179ddd14c03304a13fabbf4ecd12a09b8cc7d
InagakiKumi <k_inagaki@freestyles.jp>
2020-02-23 23:54:57 +0900

    "[2020/02/23] @1h 作戦変更画面ファイルの作成"

---
+++ Assets/Script/ChangeStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeStrategy : MonoBehaviour
+{
+    public void ButtonClick()
+    {
+                Debug.Log("「戻る」を押した");
+                SceneManager.LoadScene("BattleMain");
+
+    }
+}
