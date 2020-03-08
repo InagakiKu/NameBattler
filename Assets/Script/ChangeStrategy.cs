8555d140dd6946bdeaacfad7fbccffcc4c4ba963
InagakiKumi <k_inagaki@freestyles.jp>
2020-03-08 21:15:54 +0900

    "[2020/03/08] @1h 作戦変更画面の修正"

--- Assets/Script/ChangeStrategy.cs
+++ Assets/Script/ChangeStrategy.cs
@@ -1,24 +1,14 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Linq;
 
 public class ChangeStrategy : MonoBehaviour
 {
-    // 連携するGameObject
-    public ToggleGroup toggleGroup;
-
     public void ButtonClick()
     {
                 Debug.Log("「戻る」を押した");
-
-        // アクティブになっているToggleのラベルを取得する
-        string selectedLabel = toggleGroup.ActiveToggles()
-            .First().GetComponentsInChildren<Text>()
-            .First(t => t.name == "Label").text;
-        Debug.Log(selectedLabel);
-        SceneManager.LoadScene("BattleMain");
+                SceneManager.LoadScene("BattleMain");
 
     }
 }
