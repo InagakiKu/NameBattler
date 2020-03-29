9ae61f3b2794af51963ed9ee27b56e601363ebd2
InagakiKumi <k_inagaki@freestyles.jp>
2020-03-29 23:20:03 +0900

    "[2020/03/29] @1h キャラクター一覧画面の修正"

---
+++ Assets/Script/CharacterList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterList : MonoBehaviour
+{
+    [HideInInspector]public static string CharacterName { set; get; }
+
+    public void ButtonClick()
+    {
+        switch (transform.name)
+        {
+            case "CreateButton":
+                Debug.Log("「新しく作成する」を押した");
+                SceneManager.LoadScene("CharacterCreate");
+                break;
+            case "BackButton":
+                Debug.Log("「戻る」を押した");
+                SceneManager.LoadScene("TopScreen");
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ Assets/Script/NodeButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NodeButton : MonoBehaviour
+{
+    public string characterName;
+    //ノードのボタンクリック時に起動する
+    public void ButtonClick()
+    {
+        CharacterList.CharacterName = characterName;
+        Debug.Log("「キャラクター詳細」に遷移");
+        SceneManager.LoadScene("CharacterDetails");
+    }
+}
